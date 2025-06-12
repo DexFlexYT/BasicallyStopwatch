f57b424f14418fa75ce6b2f7687beb4389233735
DexFlex_YT <142329830+DexFlexYT@users.noreply.github.com>
2025-06-12 13:34:01 +0400

    Initial commit

---
+++ src/client/java/org/dexflex/basicallystopwatch/BasicallyStopwatchClient.java
@@ -0,0 +1,10 @@
+package org.dexflex.basicallystopwatch;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BasicallyStopwatchClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
